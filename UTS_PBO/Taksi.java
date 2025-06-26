f89fffefa939b43187c2086e94d1268bdf42a09f
JFajri <jfnew24@gmail.com>
2025-06-26 11:25:45 +0700

    Add files via upload

---
+++ UTS_PBO/Taksi.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UTS_PBO;
+
+/**
+ *
+ * @author Fajri
+ */
+/** 
+ *Junzi Fajri Denil
+ 2401082005
+ TK 1-B
+ */
+public class Taksi {
+    private int tarifAwal;
+    private int tarifperKm;
+
+    public Taksi() {
+        
+    }
+
+    public Taksi(int tarifAwal, int tarifperKm) {
+        this.tarifAwal = tarifAwal;
+        this.tarifperKm= tarifperKm;
+    }
+
+    public void setTarifAwal(int tarifAwal) {
+        this.tarifAwal = tarifAwal;
+    }
+
+    public int gettarifAwal() {
+        return tarifAwal;
+    }
+
+    public void settarifperKm(int tarifperKm) {
+        this.tarifperKm = tarifperKm;
+    }
+
+    public int gettarifperKm() {
+        return tarifperKm;
+    }
+
+    @Override
+    public String toString() {
+        return "tarif Awal: = " + tarifAwal + ", Tarif per Km = " + tarifperKm;
+    }
+}
+ 
+
---
+++ UTS_PBO/Test_Main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package UTS_PBO;
+
+/**
+ *
+ * @author Fajri
+ */
+/** 
+ *Junzi Fajri Denil
+ 2401082005
+ TK 1-B
+ */
+public class Test_Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
