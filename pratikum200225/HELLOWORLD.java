bef2cc3d530729c370fd8e06f21788f35af83a02
JFajri <jfnew24@gmail.com>
2025-06-26 11:08:00 +0700

    Add files via upload

---
+++ pratikum200225/HELLOWORLD.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fajri
+ */
+public class HELLOWORLD {
+    public static void main(String[] arqs){
+      System.out.print("HelloWorld"); //print "hello world"
+    }
+}
